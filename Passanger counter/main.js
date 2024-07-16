33a9da1a7406b115450243e50e0985b801aabc16
hikmot-kasali <kasalihikmot@gmail.com>
2024-07-16 11:58:06 +0100

    Adding

--- Passanger counter/main.js
+++ Passanger counter/main.js
@@ -3,6 +3,7 @@ let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 let count = 0
 function increment() {
+    count = 0
     count+= 1
     countEl.innerText = count
 }
@@ -12,5 +13,7 @@ function save(){
     let saveFunc = count + " - "
     saveEl.innerHTML += saveFunc
     console.log(saveFunc)
+    countEl.innerHTML = 0
+    count = 0
 }
 save()
